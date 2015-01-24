f1925d99b47cb4ee9932b4f6aee6f7d71db1a84e
Paulo Leal <paulo.leal@pleal.com.br>
2015-01-24 01:50:05 -0200

    busca e backspace

--- src/tokeninput.js
+++ src/tokeninput.js
@@ -13,6 +13,7 @@
     var HIDDEN_FIELDS_CLASS = CLASS_PREFIX + '-hidden-fields';
     var VISIBLE_LABELS_CLASS = CLASS_PREFIX + '-visible-labels';
     var DELETE_FIELD_CLASS = CLASS_PREFIX + '-delete';
+    var OPTIONS_BOX_CLASS = CLASS_PREFIX + '-options';
 
     options = $.extend({}, $.tokeninput.defaultOptions, options);
 
@@ -40,6 +41,7 @@
       var obj = $(this);
       var inputName = obj.attr('name');
 
+      var optionsBox = null;
       var block = createBlock(obj);
       var prev = obj.prev();
       obj.remove();
@@ -55,9 +57,12 @@
         return _ret;
       };
 
-      var removeEntry = function () {
+      var removeEntry = function (event, index) {
         var me = $(this);
-        var index = block.find(".tokeninput-visible-labels > li").toArray().indexOf(me.parent()[0]);
+        console.log(index);
+        if (index == null) {
+          index = block.find(".tokeninput-visible-labels > li").toArray().indexOf(me.parent()[0]);
+        }
         block.find("." + HIDDEN_FIELDS_CLASS + " > input")[index].remove();
         block.find("." + VISIBLE_LABELS_CLASS + " > li")[index].remove();
       };
@@ -73,24 +78,70 @@
         block.find("." + VISIBLE_LABELS_CLASS).append(li);
       };
 
-      block.find("." + INPUT_FIELD_CLASS).on('keydown', function (e) {
-        var keyCode = e.keyCode || e.which;
-        var interruptingKeys = [9, 188, 186, 13];
+      var updateOptionsList = function () {
+        var source = [];
 
-        if (interruptingKeys.indexOf(keyCode) >= 0) {
-          e.preventDefault();
+        var search = block.find("." + INPUT_FIELD_CLASS).val();
 
-          var target = $(e.target);
-          var displayName = "O-" + target.val() + "-O";
-          var value = $(e.target).val();
-
-          if (!findByValue(value)) {
-            addEntry(inputName, displayName, value);
-            target.val('');
-          }
+        if (typeof(options.source) === "function") {
+          source = options.source(search);
+        } else if (typeof(options.source) === "object") {
+          source = _$.grep(options.source, function (it) {
+            return it.indexOf(search) >= 0
+          });
+        } else { //Ver caso de promise
 
         }
-      });
+
+        console.log(source);
+      };
+
+      var showListIfSource = function () {
+        if (!options.source) {
+          return;
+        }
+
+        if (!optionsBox) {
+          optionsBox = createUl({class: OPTIONS_BOX_CLASS});
+        }
+      };
+
+      block.find("." + INPUT_FIELD_CLASS)
+        .on('keyup', function (e) {
+          var keyCode = e.which;
+          if (keyCode >= 48 && keyCode <= 90 || keyCode == 8 || keyCode == 46) {
+            updateOptionsList();
+          }
+        })
+        .on('keydown', function (e) {
+          var target = $(e.target);
+          var keyCode = e.which;
+          var interruptingKeys = [9, 188, 186, 13];
+
+          if (keyCode == 8) {
+            if (target.val() === "") {
+              e.preventDefault();
+              var tokensCount = block.find("." + DELETE_FIELD_CLASS).size();
+              if (tokensCount > 0) {
+                removeEntry(null, tokensCount - 1);
+              }
+            }
+          }
+
+          if (interruptingKeys.indexOf(keyCode) >= 0) {
+            e.preventDefault();
+
+            var displayName = "O-" + target.val() + "-O";
+            var value = $(e.target).val();
+
+            if (!findByValue(value)) {
+              addEntry(inputName, displayName, value);
+              target.val('');
+            }
+          }
+
+        })
+        .on('focus', showListIfSource);
 
     });
   };
