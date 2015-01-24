1db20e5d8600bb41e0d3fe18aa4c6fac736bc094
Paulo Leal <paulo.leal@pleal.com.br>
2015-01-24 16:58:35 -0200

    lista não mostra quem já está tokenizado

--- src/tokeninput.js
+++ src/tokeninput.js
@@ -5,6 +5,7 @@
 (function ($) {
   $.tokeninput = {
     defaultOptions: {
+      fromListOnly: false
     }
   };
 
@@ -15,6 +16,7 @@
     var VISIBLE_LABELS_CLASS = CLASS_PREFIX + '-visible-labels';
     var DELETE_FIELD_CLASS = CLASS_PREFIX + '-delete';
     var OPTIONS_BOX_CLASS = CLASS_PREFIX + '-options';
+    var OPTIONS_BOX_ITEMS_CLASS = CLASS_PREFIX + '-options-item';
 
     options = $.extend({}, $.tokeninput.defaultOptions, options);
 
@@ -61,7 +63,6 @@
 
       var removeEntry = function (event, index) {
         var me = $(this);
-        console.log(index);
         if (index == null) {
           index = block.find(".tokeninput-visible-labels > li").toArray().indexOf(me.parent()[0]);
         }
@@ -73,8 +74,11 @@
         var closeButton = $("<a></a>", {class: DELETE_FIELD_CLASS, href: 'javascript:void(0)'}).html('X');
         closeButton.on('click', removeEntry);
 
-        var inputObject = $("<input/>", {name: inputName, type: 'hidden', value: value});
-        var li = $("<li></li>").html(displayName).append(closeButton);
+        var inputObject = $("<input/>", {name: inputName, type: 'hidden', value: value})
+          .attr('data-displayName', displayName);
+        var li = $("<li></li>")
+          .html(displayName)
+          .append(closeButton);
 
         block.find("." + HIDDEN_FIELDS_CLASS).append(inputObject);
         block.find("." + VISIBLE_LABELS_CLASS).append(li);
@@ -90,24 +94,39 @@
         searchDelayTimer = setTimeout(function () {
           var search = block.find("." + INPUT_FIELD_CLASS).val();
           if (typeof(options.source) === "function") {
-              source = options.source(search);
-              $.when(source).done(function (result) {
-                _updateOptionsList(result);
-              });
+            source = options.source(search);
+            $.when(source).done(function (result) {
+              _updateOptionsList(result);
+            });
           } else if (typeof(options.source) === "object") {
             source = _$.grep(options.source, function (it) {
-              return it.toLowerCase().indexOf(search.toLowerCase()) >= 0
+              return it.listingName.toLowerCase().indexOf(search.toLowerCase()) >= 0
             });
             _updateOptionsList(source);
           } else {
             console.error("unrecognized source");
           }
-          console.log(source);
-        }, 200);
+        }, 100);
       };
 
       var _updateOptionsList = function (source) {
-        console.log(source);
+        optionsBox.find("li").remove();
+        $(source).each(function (index, value) {
+          if (!block.find("." + HIDDEN_FIELDS_CLASS).find("[data-displayname='" + value.displayName + "']").length) {
+
+            var el = $("<li></li>", {class: OPTIONS_BOX_ITEMS_CLASS})
+              .html(value.listingName)
+              .data('displayName', value.displayName)
+              .data('value', value.value);
+
+            el.on('click', function () {
+              var me = $(this);
+              addEntry(inputName, me.data('displayName'), me.data('value'));
+              updateOptionsList();
+            });
+            optionsBox.append(el);
+          }
+        });
       };
 
       var showListIfSource = function () {
@@ -116,8 +135,12 @@
         }
 
         if (!optionsBox) {
-          optionsBox = createUl({class: OPTIONS_BOX_CLASS});
+          optionsBox = createUl({class: OPTIONS_BOX_CLASS}).hide();
+          block.append(optionsBox);
         }
+
+        optionsBox.show();
+        updateOptionsList();
       };
 
       block.find("." + INPUT_FIELD_CLASS)
@@ -145,15 +168,16 @@
           if (interruptingKeys.indexOf(keyCode) >= 0) {
             e.preventDefault();
 
-            var displayName = "O-" + target.val() + "-O";
+            var displayName = target.val();
             var value = $(e.target).val();
 
-            if (!findByValue(value)) {
-              addEntry(inputName, displayName, value);
-              target.val('');
+            if (!options.fromListOnly) {
+              if (value.length && !findByValue(value)) {
+                addEntry(inputName, displayName, value);
+                target.val('');
+              }
             }
           }
-
         })
         .on('focus', showListIfSource);
 
